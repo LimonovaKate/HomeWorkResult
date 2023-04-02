ce66fff397ee6b3cc7e6af07a077729c4a342fd5
LimonovaKate <123876076+LimonovaKate@users.noreply.github.com>
2023-04-02 20:52:41 +0400

    Вывод в консоль в красивом виде массива

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 ﻿/* Написать программу, которая из имеющегося массива строк формирует 
 массив из строк, длинна которых меньше либо равна 3 символа. */
 
-Console.Write("Введите позиции в массиве: "); //
-string [] array=Console.ReadLine()!.Split(' ','.',',').ToArray();
+Console.Write("Введите позиции в массиве: "); //ввод данных из консоли в массив
+string [] array=Console.ReadLine()!.Split(' ','.',',').ToArray(); 
+
+Console.Write("[");  // ввыводим в консоль получившийся массив (красиво)
+for (int i=0; i<array.Length-1; i++)
+    Console.Write($"{array[i]}, ");
+Console.Write($"{array[array.Length-1]}] -> [");
